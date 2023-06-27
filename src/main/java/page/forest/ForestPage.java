8b8a9c1c0e9f9523e77a282fcf1c6da58e92c80d
qiwei.lu <qiwei.lu@b-and-qchina.com>
2023-06-27 20:41:18 +0800

    针对弹出tip统一处理

--- src/main/java/page/forest/ForestPage.java
+++ src/main/java/page/forest/ForestPage.java
@@ -12,8 +12,38 @@ public class ForestPage extends AppPage {
     private static int instanceTimes;
     //是否还有能量
     private boolean hasNext;
-
     private String who = "自己";
+    //去种树，当能量满足要求时，支付宝引导用户去种树
+    By platTrees = By.xpath("//android.widget.Button[@text='去种树']");
+    //森林新消息，第一次进入时，校验页面是否加载完成时使用
+    By newMsg = By.xpath("//android.view.View[@text='森林新消息']");
+    By ta = By.xpath("//android.view.View[contains(@text,'TA收取你')]");
+    //证书解锁，有的话需要关闭掉这个提示
+    By unlockTip = By.id("J_pop_treedialog_close");
+
+    //进入朋友的蚂蚁森林页面后，页面加载完成，左上角的标题会变成『xx的蚂蚁森林』
+    By title = By.xpath("//*[contains(@text,'的蚂蚁森林')]");
+
+    //    By revivalEnergyTip = By.xpath("//android.view.View[@text='帮好友复活能量']");
+    By confirmSendBtn = By.xpath("//android.widget.Button[contains(@text,'确认发送')]");
+    By tipCloseBtn = By.xpath("//android.widget.Button[@text='关闭']");
+    By skinTip = By.xpath("//android.view.View[contains(@text,'皮肤')]");
+    By backForestHomeBtn = By.xpath("//android.widget.Button[@text='返回我的森林']");
+
+    /**
+     * 注意，不同的手机，屏幕分辨率不同，这里的点需要重新适配
+     */
+    //能量球
+    private static final Point energy1 = new Point(150, 480);
+    private static final Point energy2 = new Point(228, 431);
+    private static final Point energy3 = new Point(324, 406);
+    private static final Point energy4 = new Point(428, 410);
+    private static final Point energy5 = new Point(492, 428);
+    private static final Point energy6 = new Point(591, 493);
+
+    //找能量
+    final Point findNext = new Point(652, 1042);
+
 
     public ForestPage(AppiumDriver driver) {
         super(driver);
@@ -35,6 +65,8 @@ public class ForestPage extends AppPage {
                 logger.info("所有好友的能量收取完成");
             }
         } else {
+            //判断『森林新消息』，确保页面加载完整
+            untilVisible(newMsg);
             this.hasNext = true;
             logger.info("进入自己的蚂蚁森林");
         }
@@ -45,43 +77,10 @@ public class ForestPage extends AppPage {
     }
 
 
-    //去种树，当能量满足要求时，支付宝引导用户去种树
-    By platTrees = By.xpath("//android.widget.Button[@text='去种树']");
-
-    By ta = By.xpath("//android.view.View[contains(@text,'TA收取你')]");
-    //证书解锁，有的话需要关闭掉这个提示
-    By unlockTip = By.id("J_pop_treedialog_close");
-
-    //进入朋友的蚂蚁森林页面后，页面加载完成，左上角的标题会变成『xx的蚂蚁森林』
-    By title = By.xpath("//*[contains(@text,'的蚂蚁森林')]");
-
-    //    By revivalEnergyTip = By.xpath("//android.view.View[@text='帮好友复活能量']");
-    By confirmSendBtn = By.xpath("//android.widget.Button[contains(@text,'确认发送')]");
-    By tipCloseBtn = By.xpath("//android.widget.Button[@text='关闭']");
-
-
-    By skinTip = By.xpath("//android.view.View[contains(@text,'皮肤')]");
-
-    By backForestHomeBtn = By.xpath("//android.widget.Button[@text='返回我的森林']");
-
-    /**
-     * 注意，不同的手机，屏幕分辨率不同，这里的点需要重新适配
-     */
-    //能量球
-    private static final Point energy1 = new Point(150, 480);
-    private static final Point energy2 = new Point(228, 431);
-    private static final Point energy3 = new Point(324, 406);
-    private static final Point energy4 = new Point(428, 410);
-    private static final Point energy5 = new Point(492, 428);
-    private static final Point energy6 = new Point(591, 493);
-
-    //找能量
-    final Point findNext = new Point(652, 1042);
-
     /**
      * 收能量
      */
-    public void getEnergy() {
+    public void getEnergy() throws Exception {
         logger.info("开始收取 {} 的能量", who);
         //围着树点6下
         getEnergy(energy1);
@@ -94,49 +93,60 @@ public class ForestPage extends AppPage {
 
     }
 
-    public void getEnergy(Point point) {
+    public void getEnergy(Point point) throws Exception {
         tap(point);
+        try {
+            WebElement element = driver.findElement(tipCloseBtn);
+            try {
+                click(element);
+                Thread.currentThread().sleep(500);
+            } catch (StaleElementReferenceException e) {
+                driver.findElement(tipCloseBtn).click();
+                Thread.currentThread().sleep(500);
+            }
+        } catch (NoSuchElementException e) {
+
+        }
         /**
          * 帮好友复活能量
          */
-        try {
-            WebElement element = driver.findElement(confirmSendBtn);
-            try {
-                click(element);
-            } catch (StaleElementReferenceException e) {
-                driver.findElement(confirmSendBtn).click();
-            }
-            Thread.currentThread().sleep(500);
-        } catch (NoSuchElementException e) {
-            //这是正常情况，因为大部分点击不会弹出『帮好友收能量』，除非好友能量过期，并且点击到了过期能量球
-        } catch (InterruptedException e) {
-
-        }
+//        try {
+//            WebElement element = driver.findElement(confirmSendBtn);
+//            try {
+//                click(element);
+//            } catch (StaleElementReferenceException e) {
+//                driver.findElement(confirmSendBtn).click();
+//            }
+//            Thread.currentThread().sleep(500);
+//        } catch (NoSuchElementException e) {
+//            //这是正常情况，因为大部分点击不会弹出『帮好友收能量』，除非好友能量过期，并且点击到了过期能量球
+//        } catch (InterruptedException e) {
+//
+//        }
         /**
          * 兼容皮肤tip
          */
-        try {
-            //xx皮肤，
-            driver.findElement(skinTip);
-            //上一步没有抛出异常，则点击X关闭皮肤tip
-            WebElement element = untilVisible(tipCloseBtn);
-            try {
-                click(element);
-            } catch (StaleElementReferenceException e) {
-                driver.findElement(tipCloseBtn).click();
-            }
-            Thread.currentThread().sleep(500);
-        } catch (NoSuchElementException e) {
-
-        } catch (InterruptedException e) {
-
-        }
+//        try {
+//            //xx皮肤，
+//            driver.findElement(skinTip);
+//            //上一步没有抛出异常，则点击X关闭皮肤tip
+//            WebElement element = untilVisible(tipCloseBtn);
+//            try {
+//                click(element);
+//            } catch (StaleElementReferenceException e) {
+//                driver.findElement(tipCloseBtn).click();
+//            }
+//            Thread.currentThread().sleep(500);
+//        } catch (NoSuchElementException e) {
+//
+//        } catch (InterruptedException e) {
+//
+//        }
 
     }
 
     public ForestPage findFriendEnergy() throws InterruptedException {
         tap(findNext);//找能量
-        Thread.currentThread().sleep(1000);
         return new ForestPage(driver);
     }
 
